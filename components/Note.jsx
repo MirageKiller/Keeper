a14544c9c2b142beefb6af44210bd642d700f921
Matt <matt.carvalho@hotmail.com>
2023-10-06 19:57:24 -0300

    Projeto Keeper - estrutura básica

---
+++ components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Note.css"
+
+function Note() {
+  return (
+    <div className="note"> 
+      <h1>Note Title</h1>
+      <p>Note content</p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+// import "./App.css";
+
+import Header from "../components/Header";
+import Note from "../components/Note";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
